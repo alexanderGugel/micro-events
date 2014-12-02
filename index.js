df34add9c46b26336df8aa594dafbf5e9b7b4ec9
Alexander Gugel <alexander.gugel@gmail.com>
2014-12-02 00:18:25 -0800

    Fix build process

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+//     micro-events 1.0.0
+//     (c) 2014 Alexander Gugel <alexander.gugel@gmail.com>
+//     micro-events may be freely distributed under the ISC license.
+
 (function () {
     function _each(arr, func, thisArg) {
         for (var i = 0; i < arr.length; i++) {
